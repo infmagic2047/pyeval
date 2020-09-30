d530051adfc72a7bc4da7f597844b39365e81fe5
Yutao Yuan <infmagic2047reg@outlook.com>
2020-10-01 02:10:23 +0800

    Better command line interface

--- src/pyeval/cmdline.py
+++ src/pyeval/cmdline.py
@@ -1,12 +1,14 @@
 import click
 
+from . import __version__
 from .code_transform import AutoImportTransformer
 from .user_shell import UserShell
 
 
 @click.command()
 @click.argument("expr")
-@click.option("--verbose", is_flag=True)
+@click.option("-v", "--verbose", is_flag=True, help="Show more information.")
+@click.version_option(version=__version__)
 def main(expr, verbose):
     user_shell = UserShell(transformers=[AutoImportTransformer(verbose=verbose)])
     user_shell.execute_code(expr)
