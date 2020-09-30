98a4404ee15f81468279cded4da555a03854fc8b
Yutao Yuan <infmagic2047reg@outlook.com>
2020-09-30 19:50:34 +0800

    Revert "Add auto import"
    This reverts commit 341650d783169ff2621a1f9500f2b1ab5f8cf8ce.

--- src/pyeval/user_shell.py
+++ src/pyeval/user_shell.py
@@ -1,20 +1,6 @@
 import ast
-import symtable
 import sys
 import traceback
-
-
-class _NameAttributeFinder(ast.NodeVisitor):
-    def visit_Attribute(self, node):
-        if not isinstance(node.value, ast.Name):
-            return self.generic_visit(node.value)
-        return [node.value.id]
-
-    def generic_visit(self, node):
-        result = []
-        for value in ast.iter_child_nodes(node):
-            result += self.visit(value)
-        return result
 
 
 class UserShell:
@@ -25,8 +11,7 @@ class UserShell:
 
     def execute_code(self, code):
         try:
-            mod, table = self._parse_code(code)
-            mod = self._transform_ast(mod, table)
+            mod = self._parse_code(code)
         except SyntaxError:
             traceback.print_exc(limit=0, file=self._print_file)
             return
@@ -49,22 +34,4 @@ class UserShell:
 
     def _parse_code(self, code):
         mod = ast.parse(code, "<input>", "exec")
-        table = symtable.symtable(code, "<input>", "exec")
-        return mod, table
-
-    def _transform_ast(self, mod, table):
-        visitor = _NameAttributeFinder()
-        names = set(visitor.visit(mod))
-        defined_symbols = set(
-            x.get_name()
-            for x in table.get_symbols()
-            if x.is_assigned() or x.is_imported() or x.is_declared_global()
-        )
-        names -= defined_symbols
-
-        # Sort the names to be predictable
-        import_names = [ast.alias(name=x, asname=None) for x in sorted(names)]
-        stmt = ast.Import(names=import_names)
-        mod.body.insert(0, stmt)
-        ast.fix_missing_locations(mod)
         return mod
